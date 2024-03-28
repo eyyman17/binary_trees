b00f3b1ff9644c62d27d5355719650690a6da580
Aymane <aymane.lamarti.8@gmail.com>
2024-03-28 13:36:10 +0000

    commit

--- binary_trees.h
+++ binary_trees.h
@@ -83,6 +83,6 @@ int *heap_to_sorted_array(heap_t *heap, size_t *size);
 int btic_helper(const binary_tree_t *tree, size_t index, size_t size);
 size_t binary_tree_size(const binary_tree_t *tree);
 
-int btia_helper(const binary_tree_t *tree, int min, int max)
+int btia_helper(const binary_tree_t *tree, int min, int max);
 
 #endif
